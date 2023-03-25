ea0bd4c67192e2daf763c39a1783abf09df49fe3
yapariel <johnarielyap143@gmail.com>
2023-03-24 20:05:14 -0800

    added products

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { Cart } from "./pages/cart/Cart";
+import { Shop } from "./pages/shop/Shop";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,78 @@
+import product1 from "./assets/1.png";
+import product2 from "./assets/2.png";
+import product3 from "./assets/3.png";
+import product4 from "./assets/4.png";
+import product5 from "./assets/5.png";
+import product6 from "./assets/6.png";
+import product7 from "./assets/7.png";
+import product8 from "./assets/8.png";
+import product9 from "./assets/9.png";
+import product10 from "./assets/10.png";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone",
+    price: 999,
+    productImage: product1,
+  },
+
+  {
+    id: 2,
+    productName: "MacBook Pro",
+    price: 999,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Earpods",
+    price: 999,
+    productImage: product3,
+  },
+
+  {
+    id: 4,
+    productName: "Keyboard and Mouse",
+    price: 999,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Headphone",
+    price: 999,
+    productImage: product5,
+  },
+
+  {
+    id: 6,
+    productName: "speakers",
+    price: 999,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Monitor",
+    price: 999,
+    productImage: product7,
+  },
+
+  {
+    id: 8,
+    productName: "Flashdrive",
+    price: 999,
+    productImage: product8,
+  },
+  {
+    id: 9,
+    productName: "Hard disk",
+    price: 999,
+    productImage: product9,
+  },
+
+  {
+    id: 10,
+    productName: "Smart Watch",
+    price: 999,
+    productImage: product10,
+  },
+];
